74da529fc6ce2444ad5049d74eef5fc6b6768a87
itay israelov <fixxx@campus.technion.ac.il>
2020-05-22 17:17:08 +0300

    mandatory is finish

--- hw3/data_preparation.py
+++ hw3/data_preparation.py
@@ -4,6 +4,7 @@ from imblearn.under_sampling import NearMiss
 from collections import Counter
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from hw3.features import *
+from sklearn.model_selection import StratifiedShuffleSplit
 
 right_feature_set = ["Vote", "Yearly_IncomeK", "Number_of_differnt_parties_voted_for", "Political_interest_Total_Score",
                      "Avg_Satisfaction_with_previous_vote", "Avg_monthly_income_all_years",
@@ -35,16 +36,16 @@ def save_files(df_train, df_test, df_validation):
 
 
 def remove_wrong_party_and_na(df_train, df_test, df_validation):
-    df_train = df_train[df_train.Vote != 10]
-    df_train = df_train[df_train.Vote != 4]
+    # df_train = df_train[df_train.Vote != 10]
+    # df_train = df_train[df_train.Vote != 4]
     df_train = df_train.dropna()
 
-    df_test = df_test[df_test.Vote != 10]
-    df_test = df_test[df_test.Vote != 4]
+    # df_test = df_test[df_test.Vote != 10]
+    # df_test = df_test[df_test.Vote != 4]
     df_test = df_test.dropna()
 
-    df_validation = df_validation[df_validation.Vote != 10]
-    df_validation = df_validation[df_validation.Vote != 4]
+    # df_validation = df_validation[df_validation.Vote != 10]
+    # df_validation = df_validation[df_validation.Vote != 4]
     df_validation = df_validation.dropna()
 
     return df_train, df_test, df_validation
@@ -58,9 +59,9 @@ def save_raw_data(df_test, df_train, df_validation):
 
 def complete_missing_values(df_train: pd.DataFrame, df_test: pd.DataFrame, df_validation: pd.DataFrame) -> (
         pd.DataFrame, pd.DataFrame, pd.DataFrame):
-    df_train = df_train[df_train > 0]
-    df_test = df_test[df_test > 0]
-    df_validation = df_validation[df_validation > 0]
+    df_train = df_train[df_train >= 0]
+    df_test = df_test[df_test >= 0]
+    df_validation = df_validation[df_validation >= 0]
 
     for col in df_train.columns.values:
         if col == 'Vote':
@@ -84,12 +85,23 @@ def complete_missing_values(df_train: pd.DataFrame, df_test: pd.DataFrame, df_va
     return df_train, df_test, df_validation
 
 
-def nominal_to_numerical_categories(df: pd.DataFrame):
+def nominal_to_numerical_categories(df_test: pd.DataFrame, df_train: pd.DataFrame, df_validation: pd.DataFrame):
     # from nominal to Categorical
-    df = df.apply(lambda x: pd.Categorical(x) if x.dtype != 'float64' else x, axis=0)
+    df_train = df_train.apply(lambda x: pd.Categorical(x) if x.dtype != 'float64' else x, axis=0)
     # give number to each Categorical
-    df = df.apply(lambda x: x.cat.codes if x.dtype != 'float64' else x, axis=0)
-    return df
+    df_train = df_train.apply(lambda x: x.cat.codes if x.dtype != 'float64' else x, axis=0)
+
+    # from nominal to Categorical
+    df_validation = df_validation.apply(lambda x: pd.Categorical(x) if x.dtype != 'float64' else x, axis=0)
+    # give number to each Categorical
+    df_validation = df_validation.apply(lambda x: x.cat.codes if x.dtype != 'float64' else x, axis=0)
+
+    # from nominal to Categorical
+    df_test = df_test.apply(lambda x: pd.Categorical(x) if x.dtype != 'float64' else x, axis=0)
+    # give number to each Categorical
+    df_test = df_test.apply(lambda x: x.cat.codes if x.dtype != 'float64' else x, axis=0)
+
+    return df_test, df_train, df_validation
 
 
 def apply_feature_selection(df_train, df_test, df_validation, feature_set):
@@ -103,25 +115,25 @@ def apply_feature_selection(df_train, df_test, df_validation, feature_set):
 def normalize(df_test: pd.DataFrame, df_train: pd.DataFrame, df_validation: pd.DataFrame):
     # min-max for uniform features
     uniform_scaler = MinMaxScaler(feature_range=(-1, 1))
-    df_train[uniform_features] = uniform_scaler.fit_transform(df_train[uniform_features])
-    df_validation[uniform_features] = uniform_scaler.transform(df_validation[uniform_features])
-    df_test[uniform_features] = uniform_scaler.transform(df_test[uniform_features])
+    df_train[uniform_features_right_features] = uniform_scaler.fit_transform(df_train[uniform_features_right_features])
+    df_validation[uniform_features_right_features] = uniform_scaler.transform(df_validation[uniform_features_right_features])
+    df_test[uniform_features_right_features] = uniform_scaler.transform(df_test[uniform_features_right_features])
 
     # z-score for normal features
     normal_scaler = StandardScaler()
-    df_train[normal_features] = normal_scaler.fit_transform(df_train[normal_features])
-    df_validation[normal_features] = normal_scaler.transform(df_validation[normal_features])
-    df_test[normal_features] = normal_scaler.transform(df_test[normal_features])
+    df_train[normal_features_right_features] = normal_scaler.fit_transform(df_train[normal_features_right_features])
+    df_validation[normal_features_right_features] = normal_scaler.transform(df_validation[normal_features_right_features])
+    df_test[normal_features_right_features] = normal_scaler.transform(df_test[normal_features_right_features])
     return df_train, df_test, df_validation
 
 
 def remove_outliers(threshold: float, df_train: pd.DataFrame, df_validation: pd.DataFrame, df_test: pd.DataFrame):
-    mean = df_train[normal_features].mean()
-    std = df_train[normal_features].std()
+    mean = df_train[normal_features_right_features].mean()
+    std = df_train[normal_features_right_features].std()
 
-    z_train = (df_train[normal_features] - mean) / std
-    z_val = (df_validation[normal_features] - mean) / std
-    z_test = (df_test[normal_features] - mean) / std
+    z_train = (df_train[normal_features_right_features] - mean) / std
+    z_val = (df_validation[normal_features_right_features] - mean) / std
+    z_test = (df_test[normal_features_right_features] - mean) / std
 
     df_train[z_train.mask(abs(z_train) > threshold).isna()] = np.nan
     df_validation[z_val.mask(abs(z_val) > threshold).isna()] = np.nan
@@ -135,13 +147,14 @@ def balanced_training_set(df_test, df_train, df_validation):
     x_train = df_train.drop("Vote", 1)
     y_train = df_train["Vote"]
     x_train_miss, y_train_miss = nr.fit_sample(x_train, y_train)
-    # counter = Counter(y_train_miss)
-    # print(counter)
-    # print("the rank of x is: ")
-    # print(x_train_miss.rank)
+
     # print("the rank of y is: ")
     # print(y_train_miss.rank)
     x_train_miss["Vote"] = y_train_miss
+    counter = Counter(x_train_miss["Vote"])
+    print("the rank of x is: ")
+    print(x_train_miss.rank)
+    print(counter)
     return x_train_miss
 
 
@@ -155,6 +168,7 @@ def main():
     """
     df = pd.read_csv("ElectionsData.csv")
 
+
     # step number 2
     # Select the right set of features (as listed in the end of this document),
     # and apply the data preparation tasks that you’ve carried out in the former
@@ -166,8 +180,6 @@ def main():
         as they should provide you added values for the modeling part
     2. Whether to use the validation set for pre-processing steps is your call
     """
-    # Convert nominal types to numerical categories
-    df = nominal_to_numerical_categories(df)
 
     # split the data to train , test and validation
     df_train, df_test, df_validation = deterministic_split(df, 0.6, 0.2)
@@ -175,11 +187,19 @@ def main():
     # Save the raw data first
     save_raw_data(df_test, df_train, df_validation)
 
+    # apply feature selection
+    feature_set = right_feature_set
+    df_train, df_test, df_validation = apply_feature_selection(df_train, df_test, df_validation, feature_set)
+
+    # Convert nominal types to numerical categories
+    df_test, df_train, df_validation = nominal_to_numerical_categories(df_test, df_train, df_validation)
+
     # 1 - Imputation - Complete missing values
     df_train, df_test, df_validation = complete_missing_values(df_train, df_test, df_validation)
 
     # 2 - Data Cleansing
     # Outlier detection using z score
+
     threshold = 3  # .3
     df_train, df_validation, df_test = remove_outliers(threshold, df_train, df_validation, df_test)
 
@@ -189,16 +209,13 @@ def main():
     # 3 - Normalization (scaling)
     df_train, df_test, df_validation = normalize(df_test, df_train, df_validation)
 
-    # apply feature selection
-    feature_set = right_feature_set
-    df_train, df_test, df_validation = apply_feature_selection(df_train, df_test, df_validation, feature_set)
-
     # balanced training set
-    df_train = balanced_training_set(df_test, df_train, df_validation)
+    # df_train = balanced_training_set(df_test, df_train, df_validation)
 
     # step number 3
     # Save the 3x2 data sets in CSV files
     # CSV files of the prepared train, validation and test data sets
+
     save_files(df_train, df_test, df_validation)
 
 
--- hw3/implements_the_modeling.py
+++ hw3/implements_the_modeling.py
@@ -1,7 +1,126 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.linear_model import SGDClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+transportation_threshold = 0.8
+
+from_num_to_label = {0: 'Blues', 1: 'Browns', 2: 'Greens', 3: 'Greys', 4: 'Khakis',
+                     5: 'Oranges', 6: 'Pinks', 7: 'Purples', 8: 'Reds',
+                     9: 'Turquoises', 10: 'Violets', 11: 'Whites', 12: 'Yellows'}
+
+from_label_to_num = {'Blues': 0, 'Browns': 1, 'Greens': 2, 'Greys': 3, 'Khakis': 4,
+                     'Oranges': 5, 'Pinks': 6, 'Purples': 7, 'Reds': 8,
+                     'Turquoises': 9, 'Violets': 10, 'Whites': 11, 'Yellows': 12}
+
+
+def winner_party(clf, x_test):
+    y_test_pred_probability = np.mean(clf.predict_proba(x_test), axis=0)
+    winner_pred = np.argmax(y_test_pred_probability)
+    print("The predicted party to win the elections is: " + from_num_to_label[winner_pred])
+    plt.plot(y_test_pred_probability)
+    plt.title("Test predicted vote probabilities")
+    plt.show()
+
+
+def print_cross_val_accuracy(sgd_clf, x_train, y_train):
+    k_folds = 10
+    cross_val_scores = cross_val_score(sgd_clf, x_train, y_train, cv=k_folds, scoring='accuracy')
+    print("accuracy in each fold:")
+    print(cross_val_scores)
+    print("mean training accuracy:")
+    print(cross_val_scores.mean())
+    print()
+
+
+def vote_division(y_pred_test, y_train):
+    pred_histogram = {}
+    for i, label in from_num_to_label.items():
+        result_true = len(y_pred_test[y_pred_test == i])
+        all_results = len(y_pred_test)
+        ratio = result_true / all_results
+        ratio_str = str(ratio * 100)
+        pred_histogram[label] = ratio_str + '%'
+    print("Pred histogram")
+    print(pred_histogram)
+
+    real_histogram = {}
+    for i, label in from_num_to_label.items():
+        num_res = len(y_train[y_train == i])
+        all_results = len(y_train)
+        ratio = num_res / all_results
+        ratio_str = str(ratio * 100)
+        real_histogram[label] = ratio_str + '%'
+    print("Real histogram")
+    print(real_histogram)
+
+    plt.hist(y_pred_test, density=True)
+    plt.title("Test predication vote division histogram")
+    plt.xlabel("The index of party")
+    plt.ylabel("Ratio of votes")
+    plt.show()
+    plt.hist(y_train, density=True)
+    plt.title("Real vote division histogram")
+    plt.xlabel("The index of party")
+    plt.ylabel("Ratio of votes")
+    plt.show()
+
+
+def save_voting_predictions(y_test_pred):
+    y_test_pred_labels = [from_num_to_label[x] for x in y_test_pred]
+    df_y_test_pred_labels = pd.DataFrame(y_test_pred_labels)
+    df_y_test_pred_labels.to_csv('test_voting_predictions.csv', index=False)
+
+
+def train_some_models(x_train, y_train):
+    print("SGDClassifier")
+    sgd_clf = SGDClassifier(random_state=92, max_iter=1000, tol=1e-3)
+    print_cross_val_accuracy(sgd_clf, x_train, y_train)
+
+    print("KNeighborsClassifier")
+    knn_clf = KNeighborsClassifier(n_neighbors=3)
+    print_cross_val_accuracy(knn_clf, x_train, y_train)
+
+    print("DecisionTreeClassifier")
+    dt_clf = DecisionTreeClassifier(random_state=0, criterion='entropy', min_samples_split=5,
+                                    min_samples_leaf=3)
+    print_cross_val_accuracy(dt_clf, x_train, y_train)
+
+    print("DecisionTreeClassifier2")
+    dt_clf = DecisionTreeClassifier(random_state=0, criterion='entropy', min_samples_split=3,
+                                    min_samples_leaf=1)
+    print_cross_val_accuracy(dt_clf, x_train, y_train)
+
+    print("RandomForestClassifier")
+    rf_clf = RandomForestClassifier(n_jobs=-1, random_state=0)
+    print_cross_val_accuracy(rf_clf, x_train, y_train)
+
+
+def calculate_overall_test_error(y_test, y_test_pred):
+    overall_test_error = 1 - len(y_test[y_test_pred == y_test]) / len(y_test)
+    print("overall test error is: ")
+    print(overall_test_error)
+
+
+def transportation_service(clf, x_test):
+    y_test_pred_probability = clf.predict_proba(x_test)
+    transport_dict = dict()
+    for index in range(13):
+        transport_dict[from_num_to_label[index]] = list()
+    for i_citizen, citizen in enumerate(y_test_pred_probability):
+        for i_label, label_probability in enumerate(citizen):
+            if label_probability > transportation_threshold:
+                transport_dict[from_num_to_label[i_label]].append(i_citizen)
+
+    print(transport_dict)
 
 
 def main():
-
     # second part - implements the modeling
     # 1. Predict which party will win the majority of votes
     # 2. Predict the division of voters between the various parties
@@ -14,6 +133,12 @@ def main():
     Each training should be done via cross-validation on the training set,
     to maximize performance of the model while avoiding over fitting.
     """
+    df_prepared_train = pd.read_csv("prepared_train.csv")
+    # shuffle
+    df_prepared_train = df_prepared_train.sample(frac=1).reset_index(drop=True)
+
+    x_train = df_prepared_train.drop("Vote", 1)
+    y_train = df_prepared_train["Vote"]
 
     # step number 5
     # Train at least two models
@@ -21,9 +146,17 @@ def main():
     Each training should be done via cross-validation on the training set,
     to maximize performance of the model while avoiding over fitting.
     """
+    # create the classifier
+    # we make the random state constant for reproducible results
+    # train (fit) using cross validation
+
+    # train_some_models(x_train, y_train)
 
     # step number 6
     # Load the prepared validation set
+    df_prepared_validation = pd.read_csv("prepared_validation.csv")
+    # shuffle
+    df_prepared_validation = df_prepared_validation.sample(frac=1).reset_index(drop=True)
 
     # step number 7
     # Apply the trained models on the validation set and check performance
@@ -31,6 +164,13 @@ def main():
         It is your call which performance measure to use,
         and it is possible to check multiple measures
     """
+    x_validation = df_prepared_validation.drop("Vote", 1)
+    y_validation = df_prepared_validation["Vote"]
+
+    # the winner! RandomForestClassifier
+    print("RandomForestClassifier on the validation set ")
+    rf_clf = RandomForestClassifier(n_jobs=-1, random_state=0)
+    # print_cross_val_accuracy(rf_clf, x_validation, y_validation)
 
     # step number 8
     # Select the best model for the prediction tasks
@@ -38,6 +178,23 @@ def main():
         The model selection is “manual” (not an automatic process),
         but it should be based on the performance measurements
     """
+    df_prepared_test = pd.read_csv("prepared_test.csv")
+    # shuffle
+    df_prepared_test = df_prepared_test.sample(frac=1).reset_index(drop=True)
+
+    x_test = df_prepared_test.drop("Vote", 1)
+    y_test = df_prepared_test["Vote"]
+
+    best_model_clf = RandomForestClassifier(n_jobs=-1, random_state=0)
+
+    x_train_and_validation = x_train.append(x_validation).reset_index(drop=True)
+    y_train_and_validation = y_train.append(y_validation).reset_index(drop=True)
+
+    # print("the best score from random forest on train + validation is:")
+    # print_cross_val_accuracy(best_model_clf, x_train_and_validation, y_train_and_validation)
+
+    best_model_clf.fit(x_train_and_validation, y_train_and_validation)
+    y_test_pred = best_model_clf.predict(x_test)
 
     # step number 9
     # Use the selected model to provide the following:
@@ -49,6 +206,26 @@ def main():
            
         2. Construct the (test) confusion matrix and overall test error
     """
+    # vote division
+    vote_division(y_test_pred, y_test)
+
+    # the party that wins the elections is:
+    print()
+    winner_party(best_model_clf, x_test)
+    print()
+
+    # save
+    save_voting_predictions(y_test_pred)
+
+    # test confusion matrix
+    plot_confusion_matrix(best_model_clf, x_test, y_test)
+    plt.show()
+
+    # overall test error
+    calculate_overall_test_error(y_test, y_test_pred)
+
+    # each party would like to suggest transportation services for its voters
+    transportation_service(best_model_clf, x_test)
 
 
 if __name__ == '__main__':
